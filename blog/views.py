f81f39b08436fcdcab586baccadee7fa3496b687
George Fleming <gflemingiii@gmail.com>
2015-01-18 15:28:24 +0000

    delete_post2

--- blog/views.py
+++ blog/views.py
@@ -7,21 +7,7 @@ from werkzeug.contrib.fixers import LighttpdCGIRootFix
 app.wsgi_app = LighttpdCGIRootFix(app.wsgi_app)
 from werkzeug import Request
 
-class MethodRewriteMiddleware(object):
-    def __init__(self, app, input_name='_method'):
-        self.app = app
-        self.input_name = input_name
 
-    def __call__(self, environ, start_response):
-        request = Request(environ)
-
-        if self.input_name in request.form:
-            method = request.form[self.input_name].upper()
-
-            if method in ['GET', 'POST', 'PUT', 'DELETE']:
-                environ['REQUEST_METHOD'] = method
-
-        return self.app(environ, start_response)
 
 @app.route("/")
 @app.route("/page/<int:page>")
